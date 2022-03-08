0a2af04b105b238d4e2d6c1869a6cca83549bdfc
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-03-08 18:37:40 +0700

    lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <headet className='header'>
+            <img src='https://www.clipartmax.com/png/full/131-1317081_sayence-0-0-reproduction-of-google-chrome-logo-by-sayence-drawing.png' />
+        </headet>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://w-dog.ru/wallpapers/14/6/324153998540799/bezmyatezhnost-gorizont-sineva.jpg' />
+            </div>
+            <div>
+                <div>
+                    <img src='https://data.whicdn.com/images/238932713/original.jpg' />
+                </div>
+                <div>
+                    description
+                </div>
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
