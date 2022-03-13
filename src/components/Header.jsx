e991da9ba1ad260be8d2cdd2d6e1d7cd156445c5
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-03-13 18:39:25 +0700

    lesson 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import h from './Header.module.css';
 
 const Header = () => {
     return (
-        <headet className='header'>
+        <headet className={h.header}>
             <img src='https://www.clipartmax.com/png/full/131-1317081_sayence-0-0-reproduction-of-google-chrome-logo-by-sayence-drawing.png' />
         </headet>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import prof from './Profile.module.css';
+//import './Profile.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={prof.content}>
             <div>
                 <img src='https://w-dog.ru/wallpapers/14/6/324153998540799/bezmyatezhnost-gorizont-sineva.jpg' />
             </div>
@@ -19,11 +21,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={prof.post}>
+                    <div className={prof.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className={prof.item}>
                         post 2
                     </div>
                 </div>
