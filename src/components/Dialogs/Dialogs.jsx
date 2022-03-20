4b7a7b4ecf430b3145a7424589268d809c11e257
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-03-20 21:46:18 +0700

    lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,16 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your do?</div>
+            </div>
         </div>
     )
 }
