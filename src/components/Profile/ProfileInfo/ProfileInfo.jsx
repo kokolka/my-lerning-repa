618f7a030d25a0f9c51c2fb3553e04458b14f5a8
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-05-02 00:54:18 +0700

    add element in info profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -13,33 +13,33 @@ import Preloader from '../../common/Preloader/Preloader';
 
 const ProfileInfo = (props) => {
 
-    let socialNetwork = (network) =>{
-        if(props.profile.contacts.facebook != null && network === 'facebook'){
+    let socialNetwork = (network) => {
+        if (props.profile.contacts.facebook != null && network === 'facebook') {
             return iconFacebook
-        } else if(props.profile.contacts.website != null && network === 'website'){
+        } else if (props.profile.contacts.website != null && network === 'website') {
             return iconWebsite
-        } else if(props.profile.contacts.vk != null && network === 'vk'){
+        } else if (props.profile.contacts.vk != null && network === 'vk') {
             return iconVK
-        } else if(props.profile.contacts.twitter != null && network === 'twitter'){
+        } else if (props.profile.contacts.twitter != null && network === 'twitter') {
             return iconTwitter
-        } else if(props.profile.contacts.instagram != null && network === 'instagram'){
+        } else if (props.profile.contacts.instagram != null && network === 'instagram') {
             return iconInstagram
-        } else if(props.profile.contacts.youtube != null && network === 'youtube'){
+        } else if (props.profile.contacts.youtube != null && network === 'youtube') {
             return iconYouTube
-        } else if(props.profile.contacts.github != null && network === 'github'){
+        } else if (props.profile.contacts.github != null && network === 'github') {
             return iconGithub
-        } else if(props.profile.contacts.mainLink != null && network === 'mainLink'){
+        } else if (props.profile.contacts.mainLink != null && network === 'mainLink') {
             return iconMainLink
-        }else{
+        } else {
             debugger
             return '';
         }
     }
 
-    if(!props.profile){
-        return <Preloader/>
+    if (!props.profile) {
+        return <Preloader />
     }
-
+    debugger
     return (
         <div className={prof.profile}>
             <div className={prof.profile_background}>
@@ -48,21 +48,37 @@ const ProfileInfo = (props) => {
             <div className={prof.profile_info}>
                 <div className={prof.profile_ava}>
                     {/* <img src='https://data.whicdn.com/images/238932713/original.jpg' /> */}
-                    <img src={props.profile.photos.large != null ? props.profile.photos.large: noPhoto} />
+                    <img src={props.profile.photos.large != null ? props.profile.photos.large : noPhoto} />
                 </div>
                 <div className={prof.profile_description}>
                     <div>{`${props.profile.fullName}`}</div>
-                    <div>{`About me: ${props.profile.aboutMe != null ? props.profile.aboutMe: ''}`}</div>
-                    <div>{`Looking for job: ${props.profile.lookingForAJob == true? 'YES': 'NO'}`}</div>
+                    <div>{`About me: ${props.profile.aboutMe != null ? props.profile.aboutMe : ''}`}</div>
+                    <div>{`Looking for job: ${props.profile.lookingForAJob == true ? 'YES' : 'NO'}`}</div>
                     <div>{`Me social network:`}
-                        <img src={socialNetwork('facebook')}/>
-                        <img src={socialNetwork('website')}/>
-                        <img src={socialNetwork('vk')}/>
-                        <img src={socialNetwork('twitter')}/>
-                        <img src={socialNetwork('instagram')}/>
-                        <img src={socialNetwork('youtube')}/>
-                        <img src={socialNetwork('github')}/>
-                        <img src={socialNetwork('mainLink')}/>
+                        <a href={props.profile.contacts.facebook} target='_blank'>
+                            <img src={socialNetwork('facebook')} />
+                        </a>
+                        <a href={props.profile.contacts.website} target='_blank'>
+                            <img src={socialNetwork('website')} />
+                        </a>
+                        <a href={props.profile.contacts.vk} target='_blank'>
+                            <img src={socialNetwork('vk')} />
+                        </a>
+                        <a href={props.profile.contacts.twitter} target='_blank'>
+                            <img src={socialNetwork('twitter')} />
+                        </a>
+                        <a href={props.profile.contacts.instagram} target='_blank'>
+                            <img src={socialNetwork('instagram')} />
+                        </a>
+                        <a href={props.profile.contacts.youtube} target='_blank'>
+                            <img src={socialNetwork('youtube')} />
+                        </a>
+                        <a href={props.profile.contacts.github} target='_blank'>
+                            <img src={socialNetwork('github')} />
+                        </a>
+                        <a href={props.profile.contacts.mainLink} target='_blank'>
+                            <img src={socialNetwork('mainLink')} />
+                        </a>
                     </div>
                 </div>
             </div>
