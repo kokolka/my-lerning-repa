7fb1fd3141081156985866ad47073c0e121d6a59
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-05-30 00:25:50 +0700

    begin change page of infinity cat

--- src/components/Infinity cat/OnlyCatPage.jsx
+++ src/components/Infinity cat/OnlyCatPage.jsx
@@ -1,15 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import s from './OnlyCatPage.module.css';
 
-let b = [];
+let b = [1, 2, 3, 1, 2, 3, 3, 3, 2, 2, 2];
+let colum = 6;
+let fotoOnColum = Math.ceil(b.length / colum);
+console.log(`fotoOnColum: ${fotoOnColum}`);
+let lastElement = 0;
+let a = 1;
 
-const CreateArrow = (arr, elements) => {
-    for (let i = 0; i < elements; i++) {
-        arr.push(Math.random() * (3 - 1) + 1);
+// const CreateArrow = (arr, elements) => {
+//     for (let i = 0; i < elements; i++) {
+//         arr.push(Math.ceil(Math.random() * (3 - 1) + 1));
+//     }
+// }
+
+const ArrFotoOnColum = () => {
+    let componentFt = [];
+    for(let j = lastElement; j < b.length; j++){
+        if(a <= fotoOnColum){
+            a = a + 1;
+            componentFt.push(<ElementFotoBox size={b[j]}/>);
+        }else{
+            a = 0;
+            lastElement = j;
+            break;
+        }
     }
+
+    return componentFt;
 }
 
-const ElementBox = (props) => {
+const ColumnsElement = () => {
+    let componentEl = []
+    for(let i = 1; i <= colum; i++){
+        componentEl.push(<div className={s.ggg}>
+            <ArrFotoOnColum />
+        </div>)
+    }
+
+    return componentEl;
+}
+
+const ElementFotoBox = (props) => {
     return <div className={props.size <= 1
         ? s.foto_block__1
         : props.size <= 2 && props.size > 1
@@ -42,27 +74,38 @@ const ElementBox = (props) => {
 
 const OnlyCatPage = () => {
 
-    let [elementForArrow, addElements] = useState(20);
-    let [oldElement, setOldElement] = useState(0);
-    CreateArrow(b, elementForArrow);
+    const n = [1, 2, 3, 4, 5, 6];
+    // let [elementForArrow, addElements] = useState(5);
+    // let [oldElement, setOldElement] = useState(0);
+    // CreateArrow(b, elementForArrow);
 
-    useEffect(() => {
-        CreateArrow(b, (elementForArrow - oldElement));
-    }, [elementForArrow])
+    // useEffect(() => {
+    //     CreateArrow(b, elementForArrow);
+    // }, [elementForArrow])
 
     return (
         <div>
             <div className={s.foto_main_block}>
-                {b.map(el => {
+                <ColumnsElement />
+                {/* {
+                    n.map(p => {
+                        return (
+                            <ColumnsElement />
+                        );
+                    })} */}
+                {/* {b.map(el => {
                     return <ElementBox size={el} />
-                })}
+                })} */}
             </div>
             <div className={s.button_block}>
-                <button className={s.button_block__element} onClick={() => {
-                    setOldElement(elementForArrow);
-                    addElements(elementForArrow + 10);
-                    alert(elementForArrow)
-                }}>
+                <button
+                    className={s.button_block__element}
+                // onClick={() => {
+                //     setOldElement(elementForArrow);
+                //     addElements(elementForArrow + 10);
+                //     alert(elementForArrow)
+                // }}
+                >
                     Add cat
                 </button>
             </div>
--- src/components/Infinity cat/OnlyCatPageContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { connect } from "react-redux";
-import OnlyCatPage from "./OnlyCatPage";
-
-
-
-export default connect()(OnlyCatPage);
