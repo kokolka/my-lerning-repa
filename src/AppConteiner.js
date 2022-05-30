450e9df0e7a5e692180b429d9f75e9771d674b8f
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-05-30 15:55:33 +0700

    lesson 95

--- src/AppConteiner.js
+++ src/AppConteiner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
 import './App.css';
@@ -8,14 +8,14 @@ import './App.css';
 const AppContainer = (props) => {
     return (
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     <App />
                     <a className='test_container'>
                         Learn React
                     </a>
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>
     );
 }
